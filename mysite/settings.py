017e661f355d15f30f8405ea9e00da8620073a29
Parth Shah <shahpv@ATRI003362.local>
2025-07-02 11:31:50 -0700

    feat(djangotraining-part5): Testing
    Added tests as per the tutorial.
    
    [ICTRAINING-2175]

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_LIST", default=[])
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "testserver",
+]
 
 
 # Application definition
--- polls/models.py
+++ polls/models.py
@@ -13,7 +13,8 @@ class Question(models.Model):
 
     # Custom method
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
