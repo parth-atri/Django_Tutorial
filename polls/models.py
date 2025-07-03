099ca72f2588e399d6f99e9443439319c79459bd
Parth Shah <shahpv@Mac.attlocal.net>
2025-07-02 21:23:09 -0700

    feat(djangotraining-part7): Customizing the admin site
    Displaying the questions model in a tabular format.
    
    [ICTRAINING-2177]

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 # Create your models here.
@@ -10,6 +11,8 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    @admin.display(boolean=True, ordering="pub_date", description="Published recently?")
 
     # Custom method
     def was_published_recently(self):
